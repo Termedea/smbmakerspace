4564b8962a3875f734ffbf8a971b0a69add34d0b
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-10-30 11:02:08 +0100

    sticky menu init

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,7 @@
-import _ from 'lodash';
+import _ from "lodash";
 
 function component() {
-  const element = document.createElement('div');
+    const element = document.createElement("div");
 
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+    return element;
 }
-
-document.body.appendChild(component());
